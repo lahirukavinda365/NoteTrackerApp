a062424141b69fb246e24f277c29d740f80a1098
lahirukavinda365 <lahirukavinda365@gmail.com>
2022-06-10 22:48:15 +0530

    complete user authentication from front end and backend

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const notes = require("./data/notes");
 const dotenv = require("dotenv");
-
+const connectDB = require("./config/db");
 const app = express();
+app.use(express.json());
 dotenv.config();
+
+const  userRoutes  = require('./routes/userRoutes');
+const { notFound, errorHandler } = require("./middleware/errorMiddleware");
+
 
 app.get("/", (req,res) => {
     res.send("API is running...")
@@ -13,14 +18,13 @@ app.get("/api/notes",(req,res) => {
     res.json(notes);
 });
 
-app.get("/api/notes/:id", (req,res) => {
-    const note = notes.find((n) => n._id === req.params.id);
-
-    res.send(note);
-});
+app.use("/api/users", userRoutes);
 
 const PORT = process.env.PORT || 5000;
+connectDB();
+
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(PORT, console.log(`Server started on ${PORT}`));
-
 
